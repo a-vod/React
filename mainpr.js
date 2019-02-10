f4ea569940dd42397f65076aeedb3c406955e532
Anastasiya <milllanoy@mail.ru>
2019-02-10 18:00:42 +0700

    Добавили измения в основных файлах проекта (работает добавление новости, добавили файл .gitignore)

--- mainpr.js
+++ mainpr.js
@@ -1,100 +1,165 @@
-var my_news = [
+'use strict';
+
+let my_tasks = [
   {
-    author: 'Саша Печкин',
-    text: 'В четчерг, четвертого числа...',
-    bigText: 'в четыре с четвертью часа четыре чёрненьких чумазеньких чертёнка чертили чёрными чернилами чертёж.'
+    name: 'Бытовые дела',
+    text: 'Почистить картошку'
   },
   {
-    author: 'Просто Вася',
-    text: 'Считаю, что $ должен стоить 35 рублей!',
-    bigText: 'А евро 42!'
-  },
-  {
-    author: 'Гость',
-    text: 'Бесплатно. Скачать. Лучший сайт - http://localhost:3000',
-    bigText: 'На самом деле платно, просто нужно прочитать очень длинное лицензионное соглашение'
+    name: 'Магазин',
+    text: 'Купить салфетки' 
   }
 ];
 
-var Article = React.createClass({
+window.ee = new EventEmitter();
+
+const Proper = React.createClass ({
   propTypes: {
-    data: React.PropTypes.shape({
-      author: React.PropTypes.string.isRequired,
-      text: React.PropTypes.string.isRequired,
-      bigText: React.PropTypes.string.isRequired
+      data: React.PropTypes.shape({
+        name: React.PropTypes.string.isRequired,
+        text: React.PropTypes.string.isRequired
     })
   },
-  getInitialState: function() {
-    return {
-      visible: false
-    };
-  },
-  readmoreClick: function(e) {
-    e.preventDefault();
-    this.setState({visible: true});
-  },
-  render: function() {
-    var author = this.props.data.author,
-        text = this.props.data.text,
-        bigText = this.props.data.bigText,
-        visible = this.state.visible;
 
+  render: function() {
+    let name = this.props.data.name,
+        text = this.props.data.text;
+        
     return (
-      <div className='article'>
-        <p className='news__author'>{author}:</p>
-        <p className='news__text'>{text}</p>
-        <a href="#"
-          onClick={this.readmoreClick}
-          className={'news__readmore ' + (visible ? 'none': '')}>
-          Подробнее
-        </a>
-        <p className={'news__big-text ' + (visible ? '': 'none')}>{bigText}</p>
+      <div className="proper">
+          <p className="task_name">{name}</p>
+          <p className="task_text">{text}</p>
       </div>
-    )
+    );
   }
 });
 
-var News = React.createClass({
+const Task = React.createClass ({
   propTypes: {
     data: React.PropTypes.array.isRequired
   },
-  render: function() {
-    var data = this.props.data;
-    var newsTemplate;
 
-    if (data.length > 0) {
-      newsTemplate = data.map(function(item, index) {
-        return (
-          <div key={index}>
-            <Article data={item} />
-          </div>
-        )
-      })
-    } else {
-      newsTemplate = <p>К сожалению новостей нет</p>
+   getInitialState: function() {
+    return {
+      counter: 0
     }
+  },
+
+  render: function() {
+    let data = this.props.data;
+    let main;
+
+     if (data.length > 0) {
+      main = data.map(function(item, index) {
+      return (
+        <div key={index}>
+          <Proper data={item} />
+        </div>
+        )
+    })
+  }
 
     return (
-      <div className='news'>
-        {newsTemplate}
-        <strong className={'news__count ' + (data.length > 0 ? '':'none') }>Всего новостей: {data.length}</strong>
+      <div className="taskclass">
+      {main}
       </div>
+      );
+  }
+});
+
+var Add = React.createClass ({
+  getInitialState: function() {
+    return {
+      nameIsEmpty: true,
+      textIsEmpty: true
+    };
+  },
+  componentDidMount: function() {
+    ReactDOM.findDOMNode(this.refs.name).focus();
+  },
+
+  onBtnClick: function (e) {
+    e.preventDefault();
+    let textEl = ReactDOM.findDOMNode(this.refs.text);
+    let name = ReactDOM.findDOMNode(this.refs.name).value;
+    let text = textEl.value;
+
+    var item = [{
+      name: name,
+      text: text
+    }];
+
+    window.ee.emit('Task.add', item);
+
+    textEl.value = '';
+    this.setState({textIsEmpty: true});
+  },
+
+   onFieldChange: function(fieldName, e) {
+    if (e.target.value.trim().length > 0) {
+      this.setState({[''+fieldName]:false})
+    } else {
+      this.setState({[''+fieldName]:true})
+    }
+  },
+
+  render: function() {
+   let nameIsEmpty = this.state.nameIsEmpty,
+       textIsEmpty = this.state.textIsEmpty;
+    
+    return (
+        <form className="add">
+          <input
+            type='text'
+            className='add_name'
+            onChange={this.onFieldChange.bind(this, 'nameIsEmpty')}
+            placeholder='Название'
+            ref='name'
+          />
+          <textarea
+            className='test_input'
+            onChange={this.onFieldChange.bind(this, 'textIsEmpty')}
+            placeholder='введите значение'
+            ref='text'>
+          </textarea>
+          <button 
+            onClick={this.onBtnClick}>Добавить задачу
+          </button>
+        </form>
     );
   }
 });
 
-var App = React.createClass({
+const App = React.createClass ({
+   getInitialState: function() {
+    return {
+      tasks: my_tasks
+    };
+  },
+  componentDidMount: function() {
+      let self = this;
+      window.ee.addListener('Task.add', function(item) {
+      let nextNews = item.concat(self.state.tasks);
+      self.setState({tasks: nextNews});
+    });
+  },
+  componentWillUnmount: function() {
+    window.ee.removeListener('Task.add');
+  },
+
   render: function() {
     return (
-      <div className='app'>
-        <h3>Новости</h3>
-        <News data={my_news} />
+      <div className="appclass">
+        <h3>ЗАДАЧИ</h3>
+        <Add />
+        <Task data={this.state.tasks} />
       </div>
-    );
+      );
   }
 });
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+ReactDOM.render (
+<App />,
+document.getElementById('root')
 );
+
